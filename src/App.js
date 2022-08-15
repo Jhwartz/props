cc880e74b16e35f8d1ae9f916179e86c578bea9f
Jhwartz <jhwartz@yahoo.co.uk>
2022-08-15 17:35:16 +0000

    Using props in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import NavBar from './components/NavBar'
+import Todo from './components/Todo'
 function App() {
+  let title ="My activities to do for the week"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar title={title} />
+      <main>
+        <Todo title="learning react" datetime="15th August 2022" />
+        <Todo title="going to the hub" datetime="16th August 2022" />
+        <Todo title="studying at the library" datetime="17th August 2022" />
+        <Todo title="taking a leisure sports" datetime="18th August 2022" />
+        <Todo title="researching about MESTAfrica" datetime="19th August 2022" />
+
+
+
+      </main>  
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
